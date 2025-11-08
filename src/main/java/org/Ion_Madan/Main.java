fd19d471b3ad01536009901c090eb9eb0e77f1ae
Ion <caroto@MacBook-Air-de-Ion.local>
2025-11-08 13:08:43 +0100

    Creacion de métodos independientes en Switch

--- src/main/java/org/Ion_Madan/Main.java
+++ src/main/java/org/Ion_Madan/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
+/*Me ha dado un error de subida y voy a volver a hacer un commit and push*/
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
