3fe1ee4ab3b50cca1c856473c55fd95cba708293
_kyleofx_ <Kyle-gi@users.noreply.github.com>
2025-11-09 20:07:35 +0100

    Cambiado paquete org.example a org.kiarash y Renombradas clases, métodos y variables al español

---
+++ src/test/java/org/kiarash/UserManagerTest.java
@@ -0,0 +1,17 @@
+package org.kiarash;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserManagerTest {
+
+    @Test
+    void probarAgregarYEncontrarUsuario() {
+        UserManager gestor = new UserManager();
+        Usuario usuario = new Usuario("kiarash", "kiarash.fst@gmail.com");
+        gestor.agregarUsuario(usuario);
+
+        assertTrue(gestor.encontrarUsuario("kiarash").isPresent());
+        assertEquals("kiarash.fst@gmail.com", gestor.encontrarUsuario("kiarash").get().getEmail());
+    }
+}
