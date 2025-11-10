5abf8b4e83dab383d53158933ad6c2fe168b23bd
MiguelRincones <mike.rincones7@gmail.com>
2025-11-10 21:41:30 +0100

    Traducción a español de todo

--- src/test/java/org/example/GestorUsuariosTest.java
+++ src/test/java/org/example/GestorUsuariosTest.java
@@ -3,13 +3,13 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UserManagerTest {
+public class GestorUsuariosTest {
 
     @Test
     void testAddAndFindUser() {
-        UserManager manager = new UserManager();
-        user user = new user("juan", "juan@example.com");
-        manager.addUser(user);
+        GestorUsuarios manager = new GestorUsuarios();
+        usuario usuario = new usuario("juan", "juan@example.com");
+        manager.addUser(usuario);
 
         assertTrue(manager.findUser("juan").isPresent());
         assertEquals("juan@example.com", manager.findUser("juan").get().getemail());
