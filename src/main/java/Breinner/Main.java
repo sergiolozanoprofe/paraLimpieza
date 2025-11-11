035a20476898d35da74b9e701261d93a616128f6
BreinnerS <breinner.iv@gmail.com>
2025-11-11 21:31:05 +0100

    Paso 2: Cambios realizados (excepto Main)

--- src/main/java/Breinner/Main.java
+++ src/main/java/Breinner/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package Breinner;
 
 import java.util.Scanner;
 
--- src/test/java/Breinner/UserManagerTest.java
+++ src/test/java/Breinner/UserManagerTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package Breinner;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
