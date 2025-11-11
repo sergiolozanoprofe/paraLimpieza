c77f503b1c58b1d7e7fe1f408ebbe2456e78bdc0
LucasMR31 <hyperpredeitor@gmail.com>
2025-11-11 17:29:30 +0100

    extracción de métodos

--- src/main/java/org/LucasMartinez/Main.java
+++ src/main/java/org/LucasMartinez/Main.java
@@ -24,21 +24,11 @@ public class Main {
             System.out.println("");
             switch (Option) {
                 case 1:
-                    System.out.print(Constantes.NOMBRE_DEL_USUARIO);
-                    String name = sc.nextLine();
-                    System.out.print(Constantes.EMAIL);
-                    String email = sc.nextLine();
-                    manager.addUser(new Usuario(name, email));
+                    extracted(sc, manager);
                     break;
                 case 2:
-                    System.out.print(Constantes.INTRODUCE_EL_NOMBRE_DE_USUARIO);
-                    String name2 = sc.nextLine();
-                    manager.findUser(name2)
-                            .ifPresentOrElse(
-                                    System.out::println,
-                                    () -> System.out.println(Constantes.USUARIO_NO_ENCONTRADO)
-                            );
-
+                    extracted1(sc, manager);
+                    break;
                 case 3:
                     manager.listUsers();
                     break;
@@ -52,5 +42,23 @@ public class Main {
         } while (Option != 0);
 
         sc.close();
+    }
+
+    private static void extracted1(Scanner sc, GestionUsuario manager) {
+        System.out.print(Constantes.INTRODUCE_EL_NOMBRE_DE_USUARIO);
+        String name2 = sc.nextLine();
+        manager.findUser(name2)
+                .ifPresentOrElse(
+                        System.out::println,
+                        () -> System.out.println(Constantes.USUARIO_NO_ENCONTRADO)
+                );
+    }
+
+    private static void extracted(Scanner sc, GestionUsuario manager) {
+        System.out.print(Constantes.NOMBRE_DEL_USUARIO);
+        String name = sc.nextLine();
+        System.out.print(Constantes.EMAIL);
+        String email = sc.nextLine();
+        manager.addUser(new Usuario(name, email));
     }
 }
