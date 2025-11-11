89437251fbb59ba49b729057973d140d861d0e3b
Sebastian Moreno <sebastianmorenot1@gmail.com>
2025-11-11 14:19:49 +0100

    extraccion de codigos del switch

--- src/main/java/org/JuanSebastianMoreno/Main.java
+++ src/main/java/org/JuanSebastianMoreno/Main.java
@@ -3,6 +3,7 @@ package org.JuanSebastianMoreno;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         usuariogerente gerente = new usuariogerente();
@@ -10,46 +11,54 @@ public class Main {
 
         do {
             System.out.println("");
-            System.out.println("1. Añadir usuario");
-            System.out.println("2. Buscar usuario");
-            System.out.println("3. Listar usuarios");
-            System.out.println("0. Salir");
-            System.out.print("Elegir: ");
+            System.out.println(constantes.AÑADIR_USUARIO);
+            System.out.println(constantes.BUSCAR_USUARIO);
+            System.out.println(constantes.LISTAR_USUARIOS);
+            System.out.println(constantes.SALIR);
+            System.out.print(constantes.ELEGIR);
             try {
                 opcion = Integer.parseInt(sc.nextLine());
             } catch (Exception e) {
-                System.out.println("Invalid input.");
+                System.out.println(constantes.INVALID_INPUT);
             }
             System.out.println("");
             switch (opcion) {
                 case 1:
-                    System.out.print("Nombre del usuario: ");
-                    String name = sc.nextLine();
-                    System.out.print("Email: ");
-                    String email = sc.nextLine();
-                    gerente.addUser(new usuario(name, email));
+                    extracted(sc, gerente);
                     break;
                 case 2:
-                    System.out.print("Introduce el nombre de usuario: ");
-                    String name2 = sc.nextLine();
-                    gerente.findUser(name2)
-                            .ifPresentOrElse(
-                                    System.out::println,
-                                    () -> System.out.println("Usuario no encontrado.")
-                            );
-
+                    extracted1(sc, gerente);
+                    break;
                 case 3:
                     gerente.listUsers();
                     break;
                 case 0:
-                    System.out.println("Bye!");
+                    System.out.println(constantes.BYE);
                     break;
                 default:
-                    System.out.println("Invalid option.");
+                    System.out.println(constantes.INVALID_OPTION);
             }
 
         } while (opcion != 0);
 
         sc.close();
+    }
+
+    private static void extracted1(Scanner sc, usuariogerente gerente) {
+        System.out.print(constantes.INTRODUCE_EL_NOMBRE_DE_USUARIO);
+        String name2 = sc.nextLine();
+        gerente.findUser(name2)
+                .ifPresentOrElse(
+                        System.out::println,
+                        () -> System.out.println(constantes.USUARIO_NO_ENCONTRADO)
+                );
+    }
+
+    private static void extracted(Scanner sc, usuariogerente gerente) {
+        System.out.print(constantes.NOMBRE_DEL_USUARIO);
+        String name = sc.nextLine();
+        System.out.print(constantes.EMAIL);
+        String email = sc.nextLine();
+        gerente.addUser(new usuario(name, email));
     }
 }
