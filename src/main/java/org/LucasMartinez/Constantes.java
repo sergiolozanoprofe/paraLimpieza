1ede89ed3ae1430c4ad6d34b7f36b838c69a5982
LucasMR31 <hyperpredeitor@gmail.com>
2025-11-11 17:27:15 +0100

    Copia a constantes

---
+++ src/main/java/org/LucasMartinez/Constantes.java
@@ -0,0 +1,16 @@
+package org.LucasMartinez;
+
+public class Constantes {
+    public static final String AÑADIR_USUARIO = "1. Añadir usuario";
+    public static final String BUSCAR_USUARIO = "2. Buscar usuario";
+    public static final String LISTAR_USUARIOS = "3. Listar usuarios";
+    public static final String SALIR = "0. Salir";
+    public static final String ELEGIR = "Elegir: ";
+    public static final String INVALID_INPUT = "Invalid input.";
+    public static final String NOMBRE_DEL_USUARIO = "Nombre del usuario: ";
+    public static final String EMAIL = "Email: ";
+    public static final String INTRODUCE_EL_NOMBRE_DE_USUARIO = "Introduce el nombre de usuario: ";
+    public static final String USUARIO_NO_ENCONTRADO = "Usuario no encontrado.";
+    public static final String BYE = "Bye!";
+    public static final String INVALID_OPTION = "Invalid option.";
+}
--- src/main/java/org/LucasMartinez/Main.java
+++ src/main/java/org/LucasMartinez/Main.java
@@ -3,6 +3,7 @@ package org.LucasMartinez;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         GestionUsuario manager = new GestionUsuario();
@@ -10,42 +11,42 @@ public class Main {
 
         do {
             System.out.println("");
-            System.out.println("1. Añadir usuario");
-            System.out.println("2. Buscar usuario");
-            System.out.println("3. Listar usuarios");
-            System.out.println("0. Salir");
-            System.out.print("Elegir: ");
+            System.out.println(Constantes.AÑADIR_USUARIO);
+            System.out.println(Constantes.BUSCAR_USUARIO);
+            System.out.println(Constantes.LISTAR_USUARIOS);
+            System.out.println(Constantes.SALIR);
+            System.out.print(Constantes.ELEGIR);
             try {
                 Option = Integer.parseInt(sc.nextLine());
             } catch (Exception e) {
-                System.out.println("Invalid input.");
+                System.out.println(Constantes.INVALID_INPUT);
             }
             System.out.println("");
             switch (Option) {
                 case 1:
-                    System.out.print("Nombre del usuario: ");
+                    System.out.print(Constantes.NOMBRE_DEL_USUARIO);
                     String name = sc.nextLine();
-                    System.out.print("Email: ");
+                    System.out.print(Constantes.EMAIL);
                     String email = sc.nextLine();
                     manager.addUser(new Usuario(name, email));
                     break;
                 case 2:
-                    System.out.print("Introduce el nombre de usuario: ");
+                    System.out.print(Constantes.INTRODUCE_EL_NOMBRE_DE_USUARIO);
                     String name2 = sc.nextLine();
                     manager.findUser(name2)
                             .ifPresentOrElse(
                                     System.out::println,
-                                    () -> System.out.println("Usuario no encontrado.")
+                                    () -> System.out.println(Constantes.USUARIO_NO_ENCONTRADO)
                             );
 
                 case 3:
                     manager.listUsers();
                     break;
                 case 0:
-                    System.out.println("Bye!");
+                    System.out.println(Constantes.BYE);
                     break;
                 default:
-                    System.out.println("Invalid option.");
+                    System.out.println(Constantes.INVALID_OPTION);
             }
 
         } while (Option != 0);
