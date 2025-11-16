5f1cc9885adba9b9af44036da74501a3838f418d
MiguelRincones <mike.rincones7@gmail.com>
2025-11-16 16:14:41 +0100

    Comentado la clase user

--- src/main/java/org/example/user.java
+++ src/main/java/org/example/user.java
@@ -2,6 +2,11 @@ package org.example;
 
 import java.util.Date;
 
+/**
+ * Creo que en esta clase es donde se guarda la información de los usuarios,
+ * y es aqui donde se accede desde la clase user manager segun la opcion que hayan elegido en el main*
+ * @author [Miguel Rincones]*/
+
 public class user {
 
     public static String COMPANY_NAME = "OpenAI";
@@ -10,14 +15,26 @@ public class user {
     private String email;
     private int age;
 
+    /**En el siguiente metodo es donde se guarda la informacion de nuevos usuarios
+     * */
+
     public user(String username, String email) {
         this.username = username;
         this.email = email;
     }
 
+    /**
+     * Aqui se da la orden de devolver el nombre de usuario
+     * */
+
     public String getUsername(){
         return username;
     }
+
+    /**
+
+     * Aqui se da la orden de devolver el email*/
+
 
     public String getemail() {
         return email;
