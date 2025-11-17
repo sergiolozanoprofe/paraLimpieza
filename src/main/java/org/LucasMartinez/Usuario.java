ab189d008e510731ac07438688fb7506cb5ab895
LucasMR31 <hyperpredeitor@gmail.com>
2025-11-17 20:49:12 +0100

    Añadidos comentarios a la clase de usuarios

--- src/main/java/org/LucasMartinez/Usuario.java
+++ src/main/java/org/LucasMartinez/Usuario.java
@@ -1,26 +1,46 @@
 package org.LucasMartinez;
 
+/**
+ * Clase que contiene info basica del usuario
+ * @author [LucasMartinezRodriguez]
+ */
 public class Usuario {
-
+    /**
+     *Nombre de compañia
+     */
     public static String COMPANY_NAME = "OpenAI";
-
+    /**
+     *Nombre de usuario
+     */
     private String username;
+    /**
+     *email
+     */
     private String email;
+    /**
+     *edad
+     */
     private int age;
 
     public Usuario(String username, String email) {
         this.username = username;
         this.email = email;
     }
-
+    /**
+     *Devuelve el nombre de usuario
+     */
     public String getUsername(){
         return username;
     }
-
+    /**
+     *devuelve el email
+     */
     public String getemail() {
         return email;
     }
-
+    /**
+     *devuelve la edad
+     */
     public String toString(){
         return "User: " + username + " (" + email + ")";
     }
