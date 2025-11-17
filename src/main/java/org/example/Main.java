4212798f30c1702039ec0588eb745baedd0d005d
rodrigomrtinez17 <rodrigo.mrtinez.17@gmail.com>
2025-11-17 20:16:50 +0100

    Ya he comentado las lineas de código

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         UserManager manager = new UserManager();
-        int Option = 0;
+        int Option = 0;//esta variable almacena la opción del menú
 
         do {
             System.out.println("");
@@ -14,41 +14,41 @@ public class Main {
             System.out.println("2. Buscar usuario");
             System.out.println("3. Listar usuarios");
             System.out.println("0. Salir");
-            System.out.print("Elegir: ");
+            System.out.print("Elegir: ");//Aqui le pide al usuario que elija una opción
             try {
                 Option = Integer.parseInt(sc.nextLine());
             } catch (Exception e) {
-                System.out.println("Invalid input.");
+                System.out.println("Invalid input.");//Aqui lee la opción del usuario y da error si no escribe un número
             }
             System.out.println("");
-            switch (Option) {
+            switch (Option) {//aqui ya entra en el switch case donde se ejecuta la opción elegida
                 case 1:
-                    System.out.print("Nombre del usuario: ");
-                    String name = sc.nextLine();
-                    System.out.print("Email: ");
-                    String email = sc.nextLine();
+                    System.out.print("Nombre del usuario: ");//Pide el nombre
+                    String name = sc.nextLine();//Guarda el nombre en un string
+                    System.out.print("Email: ");//Pide el email
+                    String email = sc.nextLine();//Guarda el email
                     manager.addUser(new user(name, email));
-                    break;
+                    break;//Termina el case 1
                 case 2:
-                    System.out.print("Introduce el nombre de usuario: ");
-                    String name2 = sc.nextLine();
+                    System.out.print("Introduce el nombre de usuario: ");//Pide el nombre
+                    String name2 = sc.nextLine();//Guarda el nombre
                     manager.findUser(name2)
                             .ifPresentOrElse(
                                     System.out::println,
-                                    () -> System.out.println("Usuario no encontrado.")
+                                    () -> System.out.println("Usuario no encontrado.")//En estas líneas si el usuario existe lo imprime y sino da error
                             );
-
+                    break;//Termina el case 2
                 case 3:
-                    manager.listUsers();
-                    break;
+                    manager.listUsers();//Muestra los usuarios registrados
+                    break;//Termina el case 3
                 case 0:
                     System.out.println("Bye!");
-                    break;
+                    break;//Muestra un mensaje y termina el menu
                 default:
-                    System.out.println("Invalid option.");
+                    System.out.println("Invalid option.");//Si el usuario no introduce un numero del 0-3 aparece esto
             }
 
-        } while (Option != 0);
+        } while (Option != 0);//Repite el menu siempre que la opcion no sea 0
 
         sc.close();
     }
