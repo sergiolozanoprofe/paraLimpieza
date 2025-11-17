32f512b5c8f8039b532638a78a9f5aed1e4992f2
Dani Gomez <danigomezteruel@gmail.com>
2025-11-17 15:59:06 +0100

    Gitignore

---
+++ target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"user"},{"p":"org.example","l":"UserManager"}];updateSearchResults();
