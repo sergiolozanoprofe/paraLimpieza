2f72c549eddf01e04949a1e659c0840f6ab9f7ae
ns2007fp-stack <ns2007fp@gmail.com>
2025-11-17 18:21:59 +0100

    comentarios añadidos de la clase user

--- src/main/java/org/example/user.java
+++ src/main/java/org/example/user.java
@@ -2,14 +2,25 @@ package org.example;
 
 import java.util.Date;
 
+/**
+ * clase q contiene informacion del usuario
+ */
 public class user {
-
+    /**
+     * nombre de la compañia
+     * nombre de usuario
+     * email
+     * edad del usuario
+     */
     public static String COMPANY_NAME = "OpenAI";
 
     private String username;
     private String email;
     private int age;
 
+    /**
+     *devuelve el nombre, email y la edad
+     */
     public user(String username, String email) {
         this.username = username;
         this.email = email;
