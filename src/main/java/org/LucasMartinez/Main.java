087129997dbc8e2b30f298fee1e7bcf61f54077b
LucasMR31 <hyperpredeitor@gmail.com>
2025-11-17 20:56:14 +0100

    Añadidos comentarios a la clase principal

--- src/main/java/org/LucasMartinez/Main.java
+++ src/main/java/org/LucasMartinez/Main.java
@@ -1,14 +1,22 @@
 package org.LucasMartinez;
-
+/**
+ *Clase principal del programa, gestiona el menú y permite registro de usuarios
+ * @author [LucasMartinezRodriguez]
+ */
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        /**
+         * Escaner para leer el texto escrito
+         */
         Scanner sc = new Scanner(System.in);
         GestionUsuario manager = new GestionUsuario();
         int Option = 0;
-
+        /**
+         *Bucle del menú
+         */
         do {
             System.out.println("");
             System.out.println(Constantes.AÑADIR_USUARIO);
@@ -17,25 +25,46 @@ public class Main {
             System.out.println(Constantes.SALIR);
             System.out.print(Constantes.ELEGIR);
             try {
+        /**
+        * Intenta convertir la entrada de un numero
+        */
                 Option = Integer.parseInt(sc.nextLine());
             } catch (Exception e) {
                 System.out.println(Constantes.INVALID_INPUT);
             }
             System.out.println("");
+        /**
+        *Seleccion de la opción del menú
+        */
             switch (Option) {
                 case 1:
+                    /**
+                     *Añade usuario y su email
+                     */
                     extracted(sc, manager);
                     break;
                 case 2:
+                    /**
+                     *Busca un usuario por su nombre
+                     */
                     extracted1(sc, manager);
                     break;
                 case 3:
+                    /**
+                     *Muestra los usuarios almacenados
+                     */
                     manager.listUsers();
                     break;
                 case 0:
+                    /**
+                     *Salir del menú
+                     */
                     System.out.println(Constantes.BYE);
                     break;
                 default:
+                    /**
+                     *Opción no válida
+                     */
                     System.out.println(Constantes.INVALID_OPTION);
             }
 
